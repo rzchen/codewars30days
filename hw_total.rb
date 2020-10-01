8cc3e1c2f17edf1865c1815b9ad283c6441a83fe
rzchen <jeff820523@gmail.com>
2020-10-01 11:24:26 +0800

    update day31

--- hw_total.rb
+++ hw_total.rb
@@ -1428,3 +1428,18 @@ RSpec.describe "RGB To Hex Conversion" do
     expect(rgb(173,255,47)).to eq('ADFF2F')
   end
 end
+
+# =================================================================
+
+def spinWords(string)
+  # 先拆成字串
+  # 判斷個別長度，大於等於5的就要 reverse
+  string.split(" ").map { |s| s.length >= 5 ? s.reverse : s }.join(" ")
+end
+
+
+RSpec.describe "Stop gninnipS My sdroW!" do
+  it "Example cases" do
+    expect(spinWords("Hey fellow warriors")).to eq("Hey wollef sroirraw")
+  end
+end
