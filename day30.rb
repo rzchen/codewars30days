de7fe3bfa6a362a371f68ead5f5d3eafeb397add
rzchen <jeff820523@gmail.com>
2020-10-01 11:23:45 +0800

    day31

--- day30.rb
+++ day30.rb
@@ -1,20 +1,16 @@
 def rgb(r, g, b)
-  # result = ""
-  # arr = [r, g, b]
+  result = ""
+  arr = [r, g, b]
 
-  # first = arr.map do |a|
-  #           a = 255 if a >= 255
-  #           a = 0 if a <= 0
-  #           a
-  #         end
+  arr.map { |a| a >= 255 ? a = 255 : a }
+     .map { |a| a <= 0 ? a = 0 : a }
+     .each do |n|
+          result += n.to_s(16).length == 1 ? "0" + n.to_s(16).upcase : n.to_s(16).upcase
+        end
 
-  # first.each do |n|
-  #         result += n.to_s(16).length == 1 ? "0" + n.to_s(16).upcase : n.to_s(16).upcase
-  #       end
+  result
 
-  # result
-
-  "%.2X%.2X%.2X" % [r,g,b].map{|i| [[i,255].min,0].max}
+  # "%.2X%.2X%.2X" % [r,g,b].map{|i| [[i,255].min,0].max}
 end
 
 RSpec.describe "RGB To Hex Conversion" do
