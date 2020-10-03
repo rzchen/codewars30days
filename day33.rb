b0f208af6f7586c629cd13a5f6c95f63844361ce
rzchen <jeff820523@gmail.com>
2020-10-03 17:47:12 +0800

    day33

---
+++ day33.rb
@@ -0,0 +1,15 @@
+def longest_palindrome s
+  # s.chars.group_by { |n| s.chars.count(n) }.max_by { |k,v| k}[1].length
+  s.match(/^((.)(?1)\2|.?)$/)
+end
+
+RSpec.describe "longest_palindrome" do
+  it "Example cases" do
+    expect(longest_palindrome("a")).to eq(1)
+    expect(longest_palindrome("aa")).to eq(2)
+    expect(longest_palindrome("baa")).to eq(2)
+    expect(longest_palindrome("aab")).to eq(2)
+    expect(longest_palindrome("baabcd")).to eq(4)
+    expect(longest_palindrome("baablkj12345432133d")).to eq(9)
+  end
+end
